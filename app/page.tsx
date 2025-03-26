e9de4ae32e1cb9ba3fcfd4632b2247aa7083bd13
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-03-26 10:06:43 +0000

    int

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <div className="flex flex-col justify-center items-center h-screen"> 
+
+{/* toast */}
+          <div className="flex items-center mb-6">
+
+            <div className="flex flex-col items-center">
+              <div className="h-[200px] ">
+
+              </div>
+              <div></div>
+            </div>
+
+          </div>
+      </div>
+  );
+}
